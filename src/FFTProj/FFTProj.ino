5798d88f0968b969719af1391d84e84125bf0e89
McBreadston <jatkinson@kingtec2169.com>
2024-04-06 10:39:05 -0500

    Did so much good golly
    complex number support complete!

--- src/FFTProj/FFTProj.ino
+++ src/FFTProj/FFTProj.ino
@@ -1,26 +1,46 @@
 //Joey Atkinson, 3/28/2024
 #include "Arduino.h"
 #include "Math.h"
-//Goal: perform a 1024 sample Radix-2 DIF FFT.
+//Goal: perform a Radix-2 DIF FFT.
 #define SAMPLING_FREQ 5000 //max frequency: 10Khz
 #define NUM_OF_BINS 1024 //max num of bins (theoretically) 4,096, should be a power of two
 #define INPUT_PIN A0 //must be an analog pin, check ATMEGA2560 datasheet for best pin
-#define PI 3.14159265359
-struct complex{
+#define PI 3.14159265359 //easier float rep of pi, easier on the atmega
+
+struct complex{ //used for complex numbers
   float r;
   float i;
 };
-
-void twiddle(struct complex *W, int N, double kn)
-{
-    W->r=cos(kn*2.0*PI/(double)N);
-    W->i=-sin(kn*2.0*PI/(double)N);
-}
-
+//look up table should be N/2 for twiddle factors
+struct complex wLookup[(NUM_OF_BINS/2)];
 
 uint8_t delayUs; //sampling frequency delay, in microseconds
-struct complex *samples;
-void fft(struct complex *samples, const int size);
+struct complex samples[NUM_OF_BINS];
+void fft(struct complex *samples, const int size); 
+
+//calculates the sum of two complex numbers
+struct complex cSum(struct complex c1, struct complex c2){
+  struct complex res;
+  res.r = c1.r + c2.r;
+  res.i = c1.i + c2.i;
+  return res;
+}
+
+//calculates the difference of two complex numbers
+struct complex cDiff(struct complex c1, struct complex c2){
+  struct complex res;
+  res.r = c1.r - c2.r;
+  res.i = c1.i - c2.i;
+  return res;
+}
+
+//calculates the product of two complex numbers
+struct complex cProd(struct complex c1, struct complex c2){
+  struct complex res;
+  res.r = (c1.r * c2.r) - (c1.i * c2.i);
+  res.i = (c1.r * c2.i) + (c1.i * c2.r);
+  return res;
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,17 +48,15 @@ void setup() {
   while(!Serial){
     ; //wait for serial monitor to initialize
   }
-
-  if ((samples=malloc(sizeof(struct complex)*(size_t)(int)NUM_OF_BINS))==NULL) //initialize data, and check if it overflows
-  {
-	  Serial.print("Out of memory!\n");
-	  while(1);
-  }
-
+  pinMode(LED_BUILTIN, OUTPUT);
   double delayTemp = (1 / SAMPLING_FREQ) * 1000000; //calculates number here to avoid truncation
   delayUs = (int) delayTemp; //the needed delay in the sampling frequency 
   delayUs -= 100; //subtract 100 microseconds to account for AnalogIn() delay
-
+  //precompute twiddle factors
+  for(int i = 0; i < (NUM_OF_BINS/2); i++){ 
+    wLookup[i].r = cos(2.0 * PI * (float)(i) / (float)(NUM_OF_BINS));
+    wLookup[i].i = sin(2.0 * PI * (float)(i) / (float)(NUM_OF_BINS));
+  }
 }
 
 void loop() {
@@ -49,15 +67,9 @@ void loop() {
     samples[i].i = 0;
     delayMicroseconds(delayUs); //needed delay in order to sample at correct sampling frequency
   }
-  fft(samples, NUM_OF_BINS);
   //TODO: calculate the radix-2 fft
   //ok so we have a list of samples... hmmmmm
 }
 
-void fft(struct complex *samples, int size){
-  //cooley tukey my hatedd 
-  if(size == 1)
-    return;
-}
 
 
