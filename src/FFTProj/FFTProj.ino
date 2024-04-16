034927e23236ae126adfc10e2574c6f5fd5e4064
McBreadston <jatkinson@kingtec2169.com>
2024-04-16 15:41:30 -0500

    Awesoem

--- src/FFTProj/FFTProj.ino
+++ src/FFTProj/FFTProj.ino
@@ -1,6 +1,7 @@
-#include <Complex.h>
-#include <stdlib.h>
-#define NUM_OF_BINS 8
+#include "Complex.h"
+//#include <stdlib.h>
+#define NUM_OF_BINS 256
+#define FREQ 10
 //I couldnt be fucked to make my own
 //complex number library
 void subFFT(Complex arr[], Complex out[], int n, int step){
@@ -49,17 +50,33 @@ void show(const char * s, Complex buf[]) {
 void loop() {
   // put your main code here, to run repeatedly:
   Complex arr[NUM_OF_BINS];
-  arr[0].set(1, 0);
-  arr[1].set(0, 0);
-  arr[2].set(-1, 0);
-  arr[3].set(0, 0);
-  arr[4].set(1, 0);
-  arr[5].set(0, 0);
-  arr[6].set(-1, 0);
-  arr[7].set(0, 0);
-  show("Data: ", arr);
+  //generate sine wave of any frequency relative to the sampling rate
+  double count = 0;
+  double inc = 2 * PI * ((double)FREQ / (double)NUM_OF_BINS);
+  Serial.println(inc);
+  //inc /= (double)NUM_OF_BINS;
+  for(int i = 0; i < NUM_OF_BINS; i++){
+
+    arr[i].set(sin(count), 0);
+    count += inc;
+  }
+  //show("Data: ", arr);
+  for(int i = 0; i < NUM_OF_BINS; i++){
+    Serial.print("Marker:");
+    Serial.print(i);
+    Serial.print(", Data:");
+    Serial.println(arr[i].real());
+    delay(10);
+  }
   FFT(arr, NUM_OF_BINS);
-  show("\nFFT : ", arr);
+  for(int i = 0; i < NUM_OF_BINS; i++){
+    Serial.print("Marker:");
+    Serial.print(i);
+    Serial.print(", Data:");
+    Serial.println(arr[i].real());
+    delay(10);
+  }
+  //show("\nFFT : ", arr);
   while(1){
     ;
   }
